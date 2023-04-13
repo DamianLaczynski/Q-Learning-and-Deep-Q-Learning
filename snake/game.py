b986b5023fab78c9ec746c6f922169776ffd3dd0
DamianLaczynski <damian.laczynski.edu@gmail.com>
2023-04-13 23:43:39 +0200

    Added tail to snake and cleaning files with .gitignore

--- snake/game.py
+++ snake/game.py
@@ -1,5 +1,4 @@
 import time
-import random
 import snakeGame
 import food
 import display
@@ -10,7 +9,7 @@ class Game:
     height = 0
     score = 0
 
-    direction = 0
+    direction = 1
 
     def __init__(self, width, height):
         self.width = width
@@ -22,15 +21,23 @@ class Game:
             for j in range(len(self.board[i])):
                 self.board[i][j] = 0
 
-        self.board[snake.x][snake.y] = 1
-        self.board[fruit.x][fruit.y] = 1
+        #dodawanie wszystkich członów węża do tablicy
+        for elem in snake.snakeList:
+            self.board[elem[1]][elem[0]] = 1
+
+        self.board[fruit.y][fruit.x] = 1
 
     def getScore(self):
         return self.score
 
+    def setDirection(self, direction):
+        self.direction = direction
+
     def isColision(self, snake, fruit):
-        if snake.x == fruit.x & snake.y == fruit.y:
+        if snake.x == fruit.x and snake.y == fruit.y:
             return True
+        else:
+            return False
 
     def isEnd(self, snake):
         if snake.isColision() == True:
@@ -49,12 +56,21 @@ class Game:
 
         #main loop
         while not game_over:
+            # sterowanie snake-m
             snake.move(self.direction)
-            self.updateBoard(snake, fruit)
-            display.display(self.board)
             if self.isColision(snake, fruit):
                 fruit.randNewPosition(self.width, self.height)
+                snake.addNewElem()
                 self.score += 1
+
+            self.updateBoard(snake, fruit)
+            #wyświetlanie planszy w konsoli
+            display.display(self.board)
+            print(self.score)
+
+            # zjedzenie owocu i zdobycie punktu
+
+            # jeśli snake połknął ogon to koniec gry
             if self.isEnd(snake) == True:
                 game_over = True
             time.sleep(1)
--- snake/snakeGame.py
+++ snake/snakeGame.py
@@ -1,27 +1,45 @@
 class SnakeClass:
     x = 0
     y = 0
-
+    snakeList = []
     board_w = 0
     board_h = 0
+
+    lastRemovedElem = []
 
     def __init__(self, start_x, start_y, width, height):
         self.x = start_x
         self.y = start_y
         self.board_w = width
         self.board_h = height
+        snakeElem = []
+        snakeElem.append(self.x)
+        snakeElem.append(self.y + 1)
+        self.snakeList.append(snakeElem)
+        snakeHead = []
+        snakeHead.append(self.x)
+        snakeHead.append(self.y)
+        self.snakeList.append(snakeHead)
+
+
 
     def isColision(self):
-        #TODO dopisać zapisywanie pozycji ogona
+        for x in self.snakeList[:-1]:
+            if x == [self.x, self.y]:
+                print(x)
+                return True
         return False
 
+    def addNewElem(self):
+        self.snakeList.insert(0, self.lastRemovedElem)
+
     def move(self, direction):
-        #move top
+        #move up
         if direction == 1:
             self.y -= 1
             if self.y < 0:
                 self.y = self.board_h - 1
-        #move bottom
+        #move down
         elif direction == 2:
             self.y += 1
             if self.y >= self.board_h:
@@ -36,3 +54,8 @@ class SnakeClass:
             self.x += 1
             if self.x >= self.board_w:
                 self.x = 0
+
+        self.snakeList.append([self.x, self.y])
+        if len(self.snakeList) > 1:
+            self.lastRemovedElem = self.snakeList[0]
+            del self.snakeList[0]
